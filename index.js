f69dee381f215c69fe8f9c1e0d281f7f87b19d05
unknown <fredericreact@gmail.com>
2022-05-31 23:23:12 +0800

    Part 162

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-document.querySelector("button").addEventListener("click",handleClick);
 
-function handleClick() {
-  alert("click");
+var numberOfDrumButtons =document.querySelectorAll(".drum").length;
+
+for (var i=0; i<numberOfDrumButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click",function () {
+    alert("click");
+});
 }
