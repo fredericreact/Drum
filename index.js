23fdded26b941f1a059bcf0728b8e6c531a9d7df
unknown <fredericreact@gmail.com>
2022-06-02 00:21:07 +0800

    Part 172

--- index.js
+++ index.js
@@ -64,6 +64,15 @@ function makeSound (key) {
 }
 
 
+
+
 function buttonAnimation (currentKey) {
+var activeButton = document.querySelector("."+currentKey)
+activeButton.classList.add("pressed");
+
+setTimeout(function() {
+
+activeButton.classList.remove("pressed");},100);
+
 
 }
